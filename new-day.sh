ad4be20fefa8b7e54067ddf69a4d5084c8ac68da
Martin Buchner <6006874+mabuchner@users.noreply.github.com>
2025-06-19 00:21:18 +0800

    Improve new-day script

--- new-day.sh
+++ new-day.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-cargo new day-08 &&\
-rm ./day-08/src/main.rs &&\
-mkdir ./day-08/src/bin &&\
-cp ./day-template/src/bin/part1.rs ./day-08/src/bin/. &&\
-cp ./day-template/.gitignore ./day-08/.
+cargo new "day-$1" &&\
+rm "./day-$1/src/main.rs" &&\
+mkdir "./day-$1/src/bin" &&\
+cp "./day-template/src/bin/part1.rs" "./day-$1/src/bin/." &&\
+touch "./day-$1/src/bin/input.txt"
+cp "./day-template/.gitignore" "./day-$1/."
