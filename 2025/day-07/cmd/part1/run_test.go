edc324fafbc3850f6ac083c0a71cc216a5ed466b
Martin Buchner <6006874+mabuchner@users.noreply.github.com>
2025-12-21 17:23:32 +0800

    Add solution for 2025 day 7

---
+++ 2025/day-07/cmd/part1/run_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRun(t *testing.T) {
+	t.Run("small input", func(t *testing.T) {
+		res, err := run("../../assets/input_small.txt")
+		assert.NoError(t, err)
+		assert.Equal(t, int64(21), res)
+	})
+
+	t.Run("normal input", func(t *testing.T) {
+		res, err := run("../../assets/input.txt")
+		assert.NoError(t, err)
+		assert.Equal(t, int64(1573), res)
+	})
+}
+
+func BenchmarkProcess(b *testing.B) {
+	inputs, err := load("../../assets/input.txt")
+	if err != nil {
+		b.FailNow()
+	}
+
+	for b.Loop() {
+		_, _ = process(inputs)
+	}
+}
